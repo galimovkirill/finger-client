2d8b2e228319bbe1f5efc58386bf19d81cfe5c34
Kirill Galimov <galimovdev@gmail.com>
2023-05-22 01:22:43 +0400

    feat: setup svg icons

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg
+        viewBox="0 0 18 18"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        :width="width"
+        :height="height"
+    >
+        <slot></slot>
+    </svg>
+</template>
+
+<script lang="ts" setup>
+export interface Props {
+    width?: number;
+    height?: number;
+}
+
+withDefaults(defineProps<Props>(), {
+    width: 18,
+    height: 18
+});
+</script>
