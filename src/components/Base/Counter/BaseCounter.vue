ad590761c5b650fc9751328ba228ed31b41717c6
Kirill Galimov <galimovdev@gmail.com>
2023-06-18 12:00:13 +0300

    feat(storybook): counter component

---
+++ src/components/Base/Counter/BaseCounter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="fg-counter" :style="{ '--counter-value': value }"></div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+    value: number;
+}
+
+defineProps<Props>();
+</script>
+
+<style lang="scss">
+@property --num {
+    syntax: '<integer>';
+    initial-value: 0;
+    inherits: false;
+}
+
+.fg-counter {
+    animation: counter 1s alternate ease;
+    animation-fill-mode: forwards;
+    counter-reset: num var(--num);
+}
+
+.fg-counter::after {
+    content: counter(num);
+}
+
+@keyframes counter {
+    from {
+        --num: 0;
+    }
+    to {
+        --num: var(--counter-value);
+    }
+}
+</style>
